6edbbec6a88e102b42b3d916eb80f6cef0645525
Shohei Aoki <shoaok@gmail.com>
2016-02-03 11:15:49 +0900

    jquery追加

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
-var senddata = require('./senddata');
-
 var express = require('express');
 var app = express();
+var path = require('path');
 
 app.get('/', function (req, res) {
-    res.send('Hello World!');
-    senddata.hoge();
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+app.post('/cholmod',function(req,res){
+  var cmpMat = req;
+  var orgMat = parse(cmpMat);
+  io.write("orgMat.tri",orgMat);
+  var L = exec("./cholmod_simple orgMat.tri");
+  var compressedL = compress(L);
+  res.send(compressedL);
+}
+
 app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+  console.log('Matrix offloader listening on port 3000...');
 });
--- senddata.js
+++ senddata.js
@@ -7,13 +7,14 @@ module.exports = {
     // var matrix = numeric.ccsSparse(compress);
     var exec = require('child_process').exec;
     var cmd = './solvematrix matrix';
-    exec(cmd, function(error, stdout, stderr) {
-        // command output is in stdout
-        // });
-  },
+    // exec(cmd, function(error, stdout, stderr) {
+    // command output is in stdout
+    // });
+    // },
+},
   readResult : function(){
-    var result = readFileSync('matrix_result.json');
-    console.log(result);
+    // var result = readFileSync('matrix_result.json');
+    console.log("hoge");
   }
-}
+};
 
